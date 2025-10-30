6e0da7b2fbd4f6590cf3c5ab35f4764bc2acebf8
ujjwak rai <ur069119@gmail.com>
2025-10-30 22:07:17 +0530

    re update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,3 +1,6 @@
+import hostel from "../assets/hostel.png";
+import todo from "../assets/todo.png";
+
 export const projects = [
   {
     id: 1,
@@ -11,7 +14,7 @@ export const projects = [
     id: 2,
     title: "HOSTEL WEBSITE",
     description: "A responsive hostel application using React and nodejs(MERN). Displays record of students and feee status, application status, room status.",
-    image: "/src/assets/hostel.png",
+    image : hostel,
     github: "https://github.com/RAIUJJWAL-555/hostel-frontend",
     demo: "https://myhosty.netlify.app/"
   },
@@ -19,7 +22,7 @@ export const projects = [
     id: 3,
     title: "Task Management Tool",
     description: "A collaborative task management application with real-time updates using React, Firebase, and Material-UI. Includes drag-and-drop functionality.",
-    image: "/src/assets/todo.png",
+    image: todo,
     github: "https://github.com/RAIUJJWAL-555/TO_DO_APP",
     demo: "https://myalltaskmanager.netlify.app/"
   }
