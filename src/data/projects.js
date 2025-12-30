9245ffe279b7dab159ada6f0ccba3288ead58b49
ujjwak rai <ur069119@gmail.com>
2025-12-30 15:57:53 +0530

    UI/UX update

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,15 +8,6 @@ import rock from "../assets/rock.png";
 import urlshort from "../assets/urlshort.png";
 
 export const projects = [
-  
-  {
-    id: 2,
-    title: "HOSTEL WEBSITE",
-    description: "A responsive hostel application using React and nodejs(MERN). Displays record of students and feee status, application status, room status.",
-    image : hostel,
-    github: "https://github.com/RAIUJJWAL-555/hostel-frontend",
-    demo: "https://myhosty.netlify.app/"
-  },
   {
     id: 1,
     title: "E-Commerce Platform",
@@ -24,12 +15,12 @@ export const projects = [
     image: image,
     github: "https://github.com/RAIUJJWAL-555/FullStack",
     demo: "https://forevershoping.vercel.app/"
-  }
-  ,{
+  },
+  {
     id: 4,
     title: "Admin pannel of e-commerce",
     description: "To add and remove items from the e-commerce website.",
-    image:admin ,
+    image: admin,
     github: "https://github.com/RAIUJJWAL-555/TO_DO_APP",
     demo: "https://full-stack-admin1.vercel.app/"
   },
@@ -43,35 +34,42 @@ export const projects = [
   },
   {
     id: 7,
-    title: "Only a portfolio samle",
+    title: "Only a portfolio portfolio",
     description: "Advance UI/UX",
     image: p2,
     github: "https://github.com/RAIUJJWAL-555/3dmodel",
     demo: "https://portfolllioo2.netlify.app/"
   },
   {
-    id: 8,
-    title: "Url Shortner",
-    description: "Just for practice and fun",
-    image:urlshort,
-    github: "https://github.com/RAIUJJWAL-555?tab=repositories",
-    demo: "https://url-shortner-eight-sable.vercel.app/login"
-  }
-  ,{
     id: 5,
     title: "Rock Paper Scessior",
     description: "Just for practice and fun",
-    image:rock ,
+    image: rock,
     github: "https://github.com/RAIUJJWAL-555?tab=repositories",
     demo: "https://stone-paper-game-xi.vercel.app/"
-  }
-  ,{
+  },
+  {
+    id: 2,
+    title: "HOSTEL WEBSITE",
+    description: "A responsive hostel application using React and nodejs(MERN). Displays record of students and feee status, application status, room status.",
+    image: hostel,
+    github: "https://github.com/RAIUJJWAL-555/hostel-frontend",
+    demo: "https://myhosty.netlify.app/"
+  },
+  {
+    id: 8,
+    title: "Url Shortner",
+    description: "Just for practice and fun",
+    image: urlshort,
+    github: "https://github.com/RAIUJJWAL-555?tab=repositories",
+    demo: "https://url-shortner-eight-sable.vercel.app/login"
+  },
+  {
     id: 6,
     title: "TIC TAC TOE",
     description: "Just for practice and fun",
-    image:tictac,
+    image: tictac,
     github: "https://github.com/RAIUJJWAL-555?tab=repositories",
     demo: "https://tic-tac-toe-vert-five-46.vercel.app/"
   }
-  
 ];
